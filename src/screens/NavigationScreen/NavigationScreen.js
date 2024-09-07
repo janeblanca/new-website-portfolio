72511232273ce8e067b0f37309bd47a5eee97de3
janeblanca <janeblancaocampo@gmail.com>
2024-09-07 12:19:49 +0800

    updated navigation bar

---
+++ src/screens/NavigationScreen/NavigationScreen.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./NavigationScreen.css"; 
+
+export default function NavigationScreen () {
+    return (
+        <header className="header">
+            <div className="webPageHomeContainer">
+                <div className="homeMyNameContainer">
+                    <span className="homeNameJane">Jane </span>
+                    <span className="homeNameBlanca">Blanca.</span>
+                </div>
+            </div>
+            <div className="webNavigationContainer">
+                <div className="webNavigation">
+                    
+                </div>
+            </div>
+        </header>
+    );
+}
