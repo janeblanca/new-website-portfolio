bbea767f1fa37d41276870b6a249337ba2db4928
janeblanca <janeblancaocampo@gmail.com>
2024-09-13 20:28:48 +0800

    updated portfolio screen

--- src/screens/PortfolioScreen/PortfolioScreen.js
+++ src/screens/PortfolioScreen/PortfolioScreen.js
@@ -1,9 +1,17 @@
 import React from "react"; 
+import "../PortfolioScreen/PortfolioScreen.css";
 
 export default function PortfolioScreen() {
     return (
-        <div>
-            
+        <div className="portfolioMainScreenContainer">
+            <div className="portfolioContainer">
+                <div className="pageTitleContainer">
+                    <span className="titleText">MY PORTFOLIO</span>
+                </div>
+                <div className="projectsContainer">
+
+                </div>
+            </div>
         </div>
     );
 }
